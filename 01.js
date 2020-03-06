9d71e4de14760d6f0ab8d44cb6e9a6ee5ac0a033
Matheus de Souza <matheus.zhsouza@gmail.com>
2020-03-06 16:52:31 -0300

    - Exercício 1 e começo do 2

---
+++ 01.js
@@ -0,0 +1,11 @@
+// Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma, subtração, multiplicação e divisão desses valores.
+
+const soma = (a, b) => console.log(a + b)
+const sub = (a, b) => console.log(a - b)
+const mult = (a, b) => console.log(a * b)
+const div = (a, b) => console.log(a / b)
+
+soma(1, 2)
+sub(25, 7)
+mult(8, 2)
+div(63, 7)
